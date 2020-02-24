0ab1740b92773fcabd3dedcc3f1ece065f94f38f
mdchvatal <58982885+mdchvatal@users.noreply.github.com>
2020-02-24 09:08:03 -0500

    Refactor paysheet_populator
    Used Requests library instead of BeautifulSoup to get site data, refactored sheet writing iterator to get data directly from json and to use cell counter for iterating across multiple loops, removed dead code.

--- timecard_app.py
+++ timecard_app.py
@@ -1,69 +1,40 @@
 import ezsheets
 import requests
-from bs4 import BeautifulSoup
 
 workdate_list = []
-names = []
-session_lengths = []
-
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36"
-}
+cell_counter = 27
 
 login_data = {
     "username": "",
     "password": "",
-
 }
 
-date = input("Paste URL of paystub sheet:  ")
-spread = ezsheets.Spreadsheet(date)
+date = input("Enter URL of pay spreadsheet:  ")
+ss = ezsheets.Spreadsheet(date)
+spread = ss[0]
 #change? url encode?
 
 cont = input("Enter a work date? Y/N  ")
 while cont == "Y":
-    schedule_date = str(input("Which date (yyyy-mm-dd)? "))
+    schedule_date = str(input("Which date? (yyyy-mm-dd)  "))
     workdate_list.append(schedule_date)
-    payload = {
-        "minDate": schedule_date,
-        "maxDate": schedule_date,
-        "calendarID": "395910"
+    cont = input("Add another day? (Y/N)  ")
 
-    }
-    for item in workdate_list:
-        req = requests.get("https://acuityscheduling.com/api/v1/appointments", params=payload, auth=(login_data["username"], login_data["password"]))
-        schedule_soup = BeautifulSoup(req.content, "html5lib")
-        print(schedule_soup)
-        #decode to json
-        name_spans = schedule_soup.find_all("#cal-parent:395910<>America/New_York appt-name")
-        #iterate from json instead
-        for name in name_spans:
-            names.append(name.get_text())
-
-        time_spans = schedule_soup.find_all("#cal-parent:395910<>America/New_York type-name")
-        #iterate from json
-        for time in time_spans:
-            session_lengths.append(time.get_text())
-
-        for name in names:
-            spread[f"B{int(names.index(name))+int(27)}"] = item
-
-        for name in names:
-            spread[f"C{int(names.index(name))+int(27)}"] = name
-
-        for session in session_lengths:
-            spread[f"D{int(session_lengths.index(item))+int(27)}"] = session
-
-        for time in session_lengths:
-            if "45" in item:
-                spread[f"E{int(session_lengths.index(item))+27}"] = .75
-            elif "60" in item:
-                spread[f"E{int(session_lengths.index(item))+27}"] = 1
-            elif "75" in item:
-                spread[f"E{int(session_lengths.index(item))+27}"] = 1.25
-            else:
-                spread[f"E{int(session_lengths.index(item))+27}"] = 1.5
-    workdate_list.pop()
-    names = []
-    session_lengths = []
-    cont = input("Enter another day?  Y/N  ")
+for date in workdate_list:
+    payload = {"minDate": date, "maxDate": date, "calendarID": "395910"}
+    req = requests.get("https://acuityscheduling.com/api/v1/appointments", params=payload, auth=(login_data["username"], login_data["password"]))
+    schedule_soup = req.json()
+    for item in schedule_soup:
+        spread[f"B{int(cell_counter)}"] = date
+        spread[f"C{int(cell_counter)}"] = str(schedule_soup[schedule_soup.index(item)]["firstName"]+" "+schedule_soup[schedule_soup.index(item)]["lastName"])
+        spread[f"D{int(cell_counter)}"] = schedule_soup[schedule_soup.index(item)]["type"]
+        if "45" in schedule_soup[schedule_soup.index(item)]["type"]:
+            spread[f"E{int(cell_counter)}"] = .75
+        elif "60" in schedule_soup[schedule_soup.index(item)]["type"]:
+            spread[f"E{int(cell_counter)}"] = 1
+        elif "75" in schedule_soup[schedule_soup.index(item)]["type"]:
+            spread[f"E{int(cell_counter)}"] = 1.25
+        else:
+            spread[f"E{int(cell_counter)}"] = 1.5
+        cell_counter += 1
+        
